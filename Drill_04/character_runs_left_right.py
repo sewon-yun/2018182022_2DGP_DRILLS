adf9706ce89974aab91af5cec9ca66bea17cc756
sewon-yun(Notebook) <55976914+sewon-yun@users.noreply.github.com>
2019-10-02 18:40:53 +0900

    Drill 04 완성
    2018182022 윤세원

---
+++ Drill_04/character_runs_left_right.py
@@ -0,0 +1,32 @@
+from pico2d import *
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+
+x = 0
+frame = 0
+motion = 0
+while True:
+    while x < 800:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 300 - motion * 100, 100, 100, x, 90)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x += 5
+        delay(0.05)
+        get_events()
+    motion = (motion + 1) % 4
+    while x > 0:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 300 - motion * 100, 100, 100, x, 90)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x -= 5
+        delay(0.05)
+        get_events()
+    motion = (motion + 1) % 4
+close_canvas()
+
