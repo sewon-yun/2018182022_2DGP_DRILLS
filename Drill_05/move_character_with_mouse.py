347d97a16fc0a0ec47091a7cd366e22ce9d5380f
sewon-yun(Notebook) <55976914+sewon-yun@users.noreply.github.com>
2019-10-07 19:13:19 +0900

    Update move_character_with_mouse.py

--- Drill_05/move_character_with_mouse.py
+++ Drill_05/move_character_with_mouse.py
@@ -6,6 +6,7 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 def handle_events():
     global running
     global x, y
+    global destination_x, destination_y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -15,14 +16,14 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
         elif event.type == SDL_MOUSEBUTTONDOWN:
-            x, y = event.x, KPU_HEIGHT - 1 - event.y
+            destination_x, destination_y = event.x, KPU_HEIGHT - 1 - event.y
     pass
 
 def draw_line(p1, p2):
     for i in range(0, 100 + 1, 2):
         t = i / 100
-        x1 = (1 - t) * p1[0] + t * p2[0]
-        y1 = (1 - t) * p1[1] + t * p2[1]
+        character_x = (1 - t) * p1[0] + t * p2[0]
+        character_y = (1 - t) * p1[1] + t * p2[1]
     pass
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
@@ -34,6 +35,8 @@ running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 character_x = 0
 character_y = 0
+destination_x = 0
+destination_y = 0
 direction = 0
 frame = 0
 hide_cursor()
@@ -42,7 +45,7 @@ while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     cursor.clip_draw(0, 0, 100, 100, x, y)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, character_x, character_y)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, destination_x, destination_y)
     update_canvas()
     frame = (frame + 1) % 8
 
