f064ca059080f5e9b4ed4529164e48a78fb46f1c
sewon-yun(Notebook) <55976914+sewon-yun@users.noreply.github.com>
2019-10-21 18:50:09 +0900

    Drill_08 10-1 완성

--- Drill_08/pause_state.py
+++ Drill_08/pause_state.py
@@ -26,7 +26,7 @@ def handle_events():
             if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
                 game_framework.quit()
             elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
-                game_framework.change_state(main_state)
+                game_framework.pop_state()
     pass
 
 
