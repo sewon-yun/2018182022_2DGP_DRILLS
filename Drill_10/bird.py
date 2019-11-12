ef557b5444c76a5c16e1758a74dacc461f7eb433
sewon-yun(Notebook) <55976914+sewon-yun@users.noreply.github.com>
2019-11-12 23:42:05 +0900

    Drill_10 최종완성
    2018182022 윤세원 commit : 3번
    https://www.youtube.com/watch?v=dVZ34Nsb3Vo

--- Drill_10/bird.py
+++ Drill_10/bird.py
@@ -4,7 +4,7 @@ from pico2d import *
 import game_world
 
 PIXEL_PER_METER = (10.0 / 0.1)  # 10 pixel 10 cm
-RUN_SPEED_KMPH = 50.0  # Km / Hour
+RUN_SPEED_KMPH = 30.0  # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
@@ -52,7 +52,7 @@ class Bird:
                 self.dir = -1
         else:
             self.velocity = -RUN_SPEED_PPS
-            if self.x < 100:
+            if self.x < 50:
                 self.dir = 1
         pass
     def draw(self):
